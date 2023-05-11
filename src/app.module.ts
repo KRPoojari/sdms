03baa069b003fc541b64bae6340e5ee7155d2732
KRPoojari <poojarikartik109@gmail.com>
2023-05-11 19:30:04 +0530

    created required modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { StudentsModule } from './modules/students/students.module';
+import { TeachersModule } from './modules/teachers/teachers.module';
+import { ClassModule } from './modules/class/class.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), StudentsModule, TeachersModule, ClassModule],
   controllers: [AppController],
   providers: [AppService],
   
