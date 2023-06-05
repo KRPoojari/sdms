9b692c1c71f9a87fbb23c1f6f0f549304fee9df9
Kartik Poojari <poojarikartik109@gmail.com>
2023-06-05 17:32:39 +0530

    Update index.ts

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -3,6 +3,6 @@ export const DEVELOPMENT = 'development';
 export const TEST = 'test';
 export const PRODUCTION = 'production';
 export const CLASS_REPOSITORY = 'CLASS_REPOSITORY'
-export const STUDENT_REPOSITORY = 'STUDEDNT_REPOSITORY'
+export const STUDENT_REPOSITORY = 'STUDENT_REPOSITORY'
 export const TEACHER_REPOSITORY = 'TEACHER_REPOSITORY'
 
